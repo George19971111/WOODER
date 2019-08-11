7277defa9118b7d321bbd48fa425e8df2ca708a9
George <salixs@mail.ru>
2019-08-11 18:27:18 +0300

    123

---
+++ www/index.php
@@ -0,0 +1,263 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>WOODS</title>
+  </head>
+  <body>
+  
+      <!--Maket na fone-->
+
+     <div class="screen" style="position: absolute; background: url(img/1.jpg) no-repeat center center fixed;"></div>
+
+       <!--Maket na fone-->
+
+
+       <header class="header01">
+                <div>
+                  <nav class="navbar navbar">
+                      <div class=" rat row">
+                        <div class="col-xs-4" id="youtube"  style="padding-left:17.2px; margin-top:22.8px; ">
+                      <img  src="img/2.png">
+                      
+                    </div>
+                     <p class="navmemuclass12">EN<img style="padding-left:5px" src="img/4.png"></p>
+                      <p class="navmemuclass">Menu</p>
+                    </div>
+                 
+
+
+                   
+                    <div class="container">
+                    <a href="#" class="navbar-brand element1"><img src="img/5.png"></a>
+                      <ul class="nav nav dfre2331">
+                        <li class="nav-item "><a href="#" class="nav-link">HOME</a></li>
+                        <li class="nav-item"  style="padding-left:90px" ><a  href="#" class="nav-link">PRODUCTS</a></li>
+                        <li class="nav-item " style="padding-left:60px; padding-right: 23px"><a href="#" class="nav-link">ABOUT</a></li>
+                      
+                      </ul>
+                        </div>
+
+
+
+
+                      <div class=" rat1 row" style="float:right">
+                        <div class="col-xs-4" style="  margin-top:22.8px; padding-right: 38px;  ">
+                      <img src="img/3.png">
+                    </div>
+                      <p class="navmemuclass1">EN<img style="padding-left:5px" src="img/4.png"></p>
+                    </div>
+                  
+                  </nav>
+                   <div id="tutub">
+                        <a href="#" >Home</a><br>
+                         <a href="#" >Products</a><br>
+                           <a href="#" >About</a>
+                </div>
+
+                </div>
+                  <div class="container-fluid">
+                  <div class="row offset-sm-2">
+                      <div class="col-xs-4 ooooi12" >
+                         <img src="img/7.png"><br>
+                        <div class="demon">
+               
+                <img src="img/8.png"><br>
+               <a href="#"> <img src="img/9.png"></a><br>
+                 <img src="img/9.png"><br>
+                  <img src="img/9.png">
+</div>
+                      </div>
+                      <div class="col-md-4">
+                       <p class="woooderfr offset-sm-7">WOODER</p>
+                      
+                       <button class="hallew offset-sm-7"><a>LEARN MORE<img src="img/10.png"></a></button> 
+                       <img class="offset-lg-7 mt-5 pl-2" src="img/11.png">
+                    
+                      </div>
+              </div>
+            </div>
+            <div class="container-fluid">
+            <img class="mr-2 mt-5 pt-5"  style="float:right;  ;" src="img/12.png">
+          </div>
+       </header>
+
+       <main class="container">
+
+         <div class="row">
+          <div class="col-xs-2" style="margin-top:186px; ">
+            <img   src="img/14.png"><br>
+            <img style="float:right; margin-top:45px; padding-right: 10px" src="img/15.png"> <br>
+            <img style="margin-top:200px" src="img/16.png">
+            <div>
+            <img class="" style="margin-top:80px; padding-left: 13px;  float:left" src="img/19.png">
+          </div>
+          </div>
+          <div class="col-xs-4">
+          <p class="mainhead">FURNITURE</p>
+          <p class="ddsaeooofllgl221123 text-left" >Wooder utilizes materials otherwise left behind,<br> rendered useless in their original intent. By creat-<br>ing new pieces made of reclaimed barnwood</p>
+          <span class="divdiv1">LEARN MORE</span>
+          <img src="img/18.png">
+          <br>
+          <img style="margin-top:-20px" src="img/17.png">
+           </div>
+             <div class="col-md-7">
+              <img style="margin-left:50px; margin-top:66px;  width:100%; height:400px;
+              " src="img/13.png"  >
+
+           </div>
+         </div>
+
+         <div class="row" style="margin-top:90px">
+          <div class="col-md-7">
+              <img style=" margin-top:66px;  width:100%; height:400px;
+              " src="img/20.png"  >
+
+           </div>
+          <div class="col-xs-3" style="margin-left:23px">
+          <p class="mainhead text-right">Decor</p>
+          <p class="ddsaeooofllgl2211234 text-right">We make all types of wooden decor as per<br> design given by Architect,Interior designer,<br> contractor which is suitable your premises. </p>
+          <span class="divdiv">LEARN MORE</span>
+          <img  src="img/18.png">
+          <br>
+          <img style="margin-top:-20px; margin-left:130px" src="img/17.png">
+           </div>
+            
+
+            <div class="col-xs-3" style="margin-top:186px;
+            margin-left:10px; ">
+            <img style="margin-left:10px"  src="img/14.png"><br>
+            <img style="float:left; margin-top:45px;margin-left:10px; " src="img/15.png"> <br>
+
+            <a href=""><img style="margin-top:200px;padding-left:50px" src="img/21.png"></a>
+            <div>
+            <img  style="margin-top:80px; float:right;" src="img/19.png">
+          </div>
+          </div>
+         </div>
+
+       </main>
+
+
+      <section class="container">
+        <p class="tttr21sa">WOODER - IS  QUALITY</p>
+
+        <center><img style="margin-top:-30px" src="img/22.png"></center>
+        <p class="textnumber">We create unique design objects made of rare wood, specially under the order.<br> Look at the short video about our work.</p>
+        <div class="row" style="margin-top:70px">
+          <div class="col-md-4">
+           <center> <img src="img/23.png"></center>
+           <center><img  style="margin-top:20px" src="img/26.png"></center>
+           <p class="headrrt21a">Innovative Design</p>
+          </div>
+            <div class="col-md-4">
+            <center><img src="img/24.png"></center>
+            <center><img  style="margin-top:20px"  src="img/26.png"></center>
+            <p class="headrrt21a">High-level Skills</p>
+          </div>
+            <div class="col-md-4">
+           <center><img src="img/25.png"></center>
+           <center><img  style="margin-top:20px"  src="img/26.png"></center>
+           <p class="headrrt21a">Quality Product</p>
+          </div>
+          
+        </div>
+
+      </section>
+      <section class="mother">
+          <div  class="father">
+              <div class="container">
+                  <div class="row" style="margin-top: 200px">
+                    <div class="col-xs-2">
+                      <img style="margin-top:30px" src="img/28.png"><br>
+                      <img style="float:right; margin-top:135px" src="img/29.png">
+                    </div>
+                      <div class="col-md-8" style="margin-left:20px">
+                      <p class="fdffoooolklk">WE DO THE DESIGN<br> OF ANY COMPLEXITY</p>
+                      <p class="sadasdsaasgo21331111">This is a team of professionals that make the furniture and<br>
+wood décor high standard. Creating modern designs.<br>
+Adhering to the global quality standards. And we are<br>
+doing work with love.</p>
+        <button class="dddldldlwwwer"><img src="img/31.png"><span>WATCH VIDEO</span></button>
+                    </div>
+                     <div class="col-xs-1" style="margin-left:200px; margin-top:80px" >
+                      <img src="img/30.png">
+                    </div>
+                    
+                  </div>
+              </div>
+          </div>
+      </section>
+
+
+      <section class="container" style="margin-top:130px">
+        <div class="row">
+          <div class="col-md-7">
+                <img src="img/32.png">
+          </div>
+          <div class="col-md-5" style="padding-left: 50px">
+           <p class="mainhead12">ABOUT US</p>
+          <p class="ddsaeooofllgl22112365 text-left" >We are a team of professionals in the wood pro-<br> cessing and the creation of wooden furniture<br> 
+of the highest class</p>
+          <span class="divdiv1">LEARN MORE</span>
+          <img src="img/18.png">
+          <br>
+          <img style="margin-top:-20px" src="img/17.png">
+          </div>
+        </div>
+        
+      </section>
+      <footer class="footerok" style="margin-top:107px">
+        <div class="container">
+        <div class="row" >
+
+          <div class="col-md-4" style="padding-top:25px">
+            <p class="preprefoot">&copy; <?php echo date ('Y')?> WOODER</p>  
+          </div>
+           <div class="col-md-4" style="padding-top:20px">
+              <img src="img/33.png">
+          </div>
+           <div class="col-md-4" style="padding-top:25px">
+              <p class="peter21">Disigned by <span style="color: rgba(255,255,255,0.8)">Viacheslav Ollanishyn</span></p>
+          </div>
+          
+        </div>
+</div>
+      </footer>
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      
+        
+        
+        
+    <script src="js/main.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
